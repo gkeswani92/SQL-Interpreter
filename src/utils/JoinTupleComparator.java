8579e1143d2a91140ea22b24c5e9478fe8c97b8b
Tanvi Mehta <tanvimmehta@gmail.com>
2015-11-24 19:59:49 -0500

    Added null check to tuple comparator.

--- src/utils/JoinTupleComparator.java
+++ src/utils/JoinTupleComparator.java
@@ -22,8 +22,13 @@ public class JoinTupleComparator {
 	 */
 	public int joinCompare(Tuple o1, Tuple o2) {
 		int n = 10;
+		//TODO: MAKE SURE THIS IS SUPPOSED TO BE HERE!!!!!
+		if (o1 == null || o2 == null) {
+			return -1;
+		}
 		
 		for (int i = 0; i < leftColumns.size(); i++) {
+
 			Integer left = o1.getValueForAttr(leftColumns.get(i).toString());
 			
 			for (int j = 0; j < rightColumns.size(); j++) {
